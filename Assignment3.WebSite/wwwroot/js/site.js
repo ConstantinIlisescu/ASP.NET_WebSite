d47be38459141e67da9b2a328f1785449d3c7ddb
ConstantinIlisescu <66800029+ConstantinIlisescu@users.noreply.github.com>
2022-04-22 20:08:18 +0100

    Add project files.

---
+++ Assignment3.WebSite/wwwroot/js/site.js
@@ -0,0 +1,17 @@
+﻿// Please see documentation at https://docs.microsoft.com/aspnet/core/client-side/bundling-and-minification
+// for details on configuring this project to bundle and minify static web assets.
+
+// Write your JavaScript code.
+
+
+const target = document.querySelector("iframe.video-item");
+const trigger = document.querySelector("button.trigger");
+
+trigger.addEventListener("click", (e) => {
+    e.preventDefault();
+        target.classList.add("apply-shake");
+});
+
+target.addEventListener("animationend", (e) => {
+    target.classList.remove("apply-shake");
+});
